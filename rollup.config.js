49b560e194539e0a1c4f3e5021feaf0a3de0be91
Stian Westvig <stian.westvig@gmail.com>
2017-06-18 01:47:40 +0200

    added babel transpilation

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,14 @@
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
 export default {
   entry: 'web/js/main.js',
   format: 'cjs',
+  plugins: [
+    resolve(),
+    babel({
+      exclude: ['node_modules/**', 'web/dist/**']
+    })
+  ],
   dest: 'web/dist/bundle.js'
 };
--- web/js/three.js
+++ web/js/three.js
@@ -1,3 +1,5 @@
 export function printThree() {
-  console.log('Loaded three.js');
+  console.log('Loaded three.js - using some es6 features:');
+  [1,2,3,4].map((number => console.log(`Counting: ${number}...`)));
 }
+
